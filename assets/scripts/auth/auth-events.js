35febc4678330fab3b0f999abdb835502f78f687
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-01-29 13:14:34 -0500

    Comment out auth-events file

--- assets/scripts/auth/auth-events.js
+++ assets/scripts/auth/auth-events.js
@@ -1,54 +1,54 @@
-'use strict'
-const api = require('./../api')
-const ui = require('./../ui')
-const getFormFields = require('./../../../lib/get-form-fields')
-
-const onSignUp = function (event) {
-  event.preventDefault()
-
-  const form = event.target
-  const data = getFormFields(form)
-
-  api.signUp(data)
-    .then(ui.signUpSuccess)
-    .catch(ui.signUpFailure)
-}
-
-const onSignIn = function (event) {
-  event.preventDefault()
-
-  const form = event.target
-  const data = getFormFields(form)
-
-  api.signIn(data)
-    .then(ui.signInSuccess)
-    .catch(ui.signInFailure)
-}
-
-const onChangePw = function (event) {
-  event.preventDefault()
-
-  const form = event.target
-  const data = getFormFields(form)
-  api.changePw(data)
-    .then(ui.changePwSuccess)
-    .catch(ui.changePwFailure)
-}
-
-const onLogout = function (event) {
-  event.preventDefault()
-  api.signOut()
-    .then(ui.signOutSuccess)
-    .catch(ui.signOutFailure)
-}
-
-const addHandlers = function () {
-  $('#sign-up').on('submit', onSignUp)
-  $('#sign-in').on('submit', onSignIn)
-  $('#change-pw').on('submit', onChangePw)
-  $('#logout').on('sumbit', onLogout)
-}
-
-module.exports = {
-  addHandlers
-}
+// 'use strict'
+// const api = require('./../api')
+// const ui = require('./../ui')
+// const getFormFields = require('./../../../lib/get-form-fields')
+//
+// const onSignUp = function (event) {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const data = getFormFields(form)
+//
+//   api.signUp(data)
+//     .then(ui.signUpSuccess)
+//     .catch(ui.signUpFailure)
+// }
+//
+// const onSignIn = function (event) {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const data = getFormFields(form)
+//
+//   api.signIn(data)
+//     .then(ui.signInSuccess)
+//     .catch(ui.signInFailure)
+// }
+//
+// const onChangePw = function (event) {
+//   event.preventDefault()
+//
+//   const form = event.target
+//   const data = getFormFields(form)
+//   api.changePw(data)
+//     .then(ui.changePwSuccess)
+//     .catch(ui.changePwFailure)
+// }
+//
+// const onLogout = function (event) {
+//   event.preventDefault()
+//   api.signOut()
+//     .then(ui.signOutSuccess)
+//     .catch(ui.signOutFailure)
+// }
+//
+// const addHandlers = function () {
+//   $('#sign-up').on('submit', onSignUp)
+//   $('#sign-in').on('submit', onSignIn)
+//   $('#change-pw').on('submit', onChangePw)
+//   $('#logout').on('sumbit', onLogout)
+// }
+//
+// module.exports = {
+//   addHandlers
+// }
