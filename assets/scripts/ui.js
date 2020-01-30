acae23b10a6511aa955c00818df7fc6af1a635a7
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-01-29 19:22:59 -0500

    UI save

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -3,12 +3,12 @@ const store = require('./store')
 
 const signUpSuccess = function (response) {
   $('#signUpMessage').text('successfully signed up!')
-  // $('#sign-up').trigger('reset')
+  $('#sign-up').trigger('reset')
 }
 
 const signUpFailure = function () {
   $('#signUpMessage').text('sign up failure!')
-  // $('#sign-up').trigger('reset')
+  $('#sign-up').trigger('reset')
 }
 
 const signInSuccess = function (response) {
@@ -16,16 +16,36 @@ const signInSuccess = function (response) {
   store.user = response.user
   $('#sign-up-modal').hide()
   $('#sign-in-modal').hide()
+  $('#changepw-modal').show()
+  $('#createNewBook').show()
+  $('#logout').show()
+  $('#sign-in').trigger('reset')
 }
 
 const signInFailure = function () {
   $('#signInMessage').text('Unable to Sign In!')
-  // $('#sign-in').trigger('reset')
+  $('#sign-in').trigger('reset')
+}
+
+const signOutSuccess = function (response) {
+  $('#logoutMessage').text('successfully signed out!')
+  $('#sign-in-modal').trigger('reset')
+  $('#changepw-modal').hide()
+  $('#logout').hide()
+  $('#sign-up-modal').show()
+  $('#sign-in-modal').show()
+  $('#createNewBook').hide()
+}
+
+const signOutFailure = function () {
+  $('#logoutMessage').text('could not sign out!')
 }
 
 module.exports = {
   signUpSuccess,
   signUpFailure,
   signInSuccess,
-  signInFailure
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
 }
